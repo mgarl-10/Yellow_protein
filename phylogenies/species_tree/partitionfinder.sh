6e3a0c36844db4fba86205c68fab677894b884bf
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-09-16 17:30:55 +0200

    Create partitionfinder.sh

---
+++ phylogenies/species_tree/partitionfinder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N pf_sym_nucl
+#$ -cwd
+
+set NUMEXPR_MAX_THREADS=128
+
+source ~/.bashrc
+conda activate py27
+set NUMEXPR_MAX_THREADS=128
+
+python /ebio/ag-salem/projects/Metagenomic_assemblies/code/partitionfinder-2.1.1/PartitionFinder.py --raxml partitionfinder -p 128 --rcluster-max 100
