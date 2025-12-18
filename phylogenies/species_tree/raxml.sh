db98b941bcafb9ef152154b550a5b1ea36647831
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:28:58 +0100

    Update raxml.sh

--- phylogenies/species_tree/raxml.sh
+++ phylogenies/species_tree/raxml.sh
@@ -4,6 +4,6 @@
 #$ -cwd
 
 
-/ebio/ag-salem/projects/Metagenomic_assemblies/code/raxml-ng_v1.2.0_linux_x86_64_MPI/bin/raxml-ng-mpi -all -msa concatenated_alignment_7species.phy --prefix host_7species_unrooted --threads 16 -model partitions.txt --tree pars{25},rand{25} 
+raxml-ng-mpi -all -msa concatenated_alignment_7species.phy --prefix host_7species_unrooted --threads 16 -model partitions.txt --tree pars{25},rand{25} 
 
 
