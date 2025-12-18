2f65b262655bc1693f1a9e154879f78403bb38f2
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:28:24 +0100

    Update modeltest.sh

--- phylogenies/Yellow_protein_tree/modeltest.sh
+++ phylogenies/Yellow_protein_tree/modeltest.sh
@@ -4,6 +4,6 @@
 #$ -cwd
 
 
-/ebio/ag-salem/projects/Metagenomic_assemblies/code/modeltest/bin/modeltest-ng -i yellow_filtered_coleopt_hymenopt_chely_bact_Drosophila_renamed_alignment.phy -p 16 -d aa -T raxml
+modeltest-ng -i yellow_filtered_coleopt_hymenopt_chely_bact_Drosophila_renamed_alignment.phy -p 16 -d aa -T raxml
 
 
