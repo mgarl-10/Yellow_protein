94fbaa19a9bd532f59d0ff3b4568d8609fd41a72
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:27:39 +0100

    Update align_muscle_seq.sh

--- phylogenies/Stammera_phylogeny/align_muscle_seq.sh
+++ phylogenies/Stammera_phylogeny/align_muscle_seq.sh
@@ -2,5 +2,5 @@
 
 for i in *yellow_species.fasta
 do tag=${i%yellow_species.fasta}
-/Users/mgarcialozano/code/muscle-5.1/src/Darwin/muscle -align "$i" -output "$tag"alignment.fasta
+muscle -align "$i" -output "$tag"alignment.fasta
 done 
