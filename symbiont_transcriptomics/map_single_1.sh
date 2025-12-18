0c59ef35e6659284b914423c5de485bc5f332b09
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:35:01 +0100

    Update map_single_1.sh

--- symbiont_transcriptomics/map_single_1.sh
+++ symbiont_transcriptomics/map_single_1.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N map1_sing
+#$ -N map_sing
 #$ -cwd
 
 source ~/.bashrc
 conda activate bowtie2
 
-bowtie2 -x Stammera -U CTL_UNT_R1_trimmed_single_1.fastq.gz --fr --no-unal -p 64 -S CTL_UNT_R1_Stammera_single.sam
+INDEX=Stammera
 
-bowtie2 -x Stammera -U CTL_HUM_R1_trimmed_single_1.fastq.gz --fr --no-unal -p 64 -S CTL_HUM_R1_Stammera_single.sam
+for read in *_trimmed_single_1.fastq.gz; do
+    sample=$(basename "$read" _trimmed_single_1.fastq.gz)
+
+    echo "Mapping $sample"
+
+    bowtie2 \
+      -x "$INDEX" \
+      -U "$read" \
+      --fr \
+      --no-unal \
+      -p 64 \
+      -S "${sample}_Stammera_single.sam"
+done
