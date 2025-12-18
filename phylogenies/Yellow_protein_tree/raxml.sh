74fbe1ae4ce6556fe1f81675ba8cd51ae5919ec8
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:28:34 +0100

    Update raxml.sh

--- phylogenies/Yellow_protein_tree/raxml.sh
+++ phylogenies/Yellow_protein_tree/raxml.sh
@@ -4,6 +4,6 @@
 #$ -cwd
 
 
-/ebio/ag-salem/projects/Metagenomic_assemblies/code/raxml-ng_v1.2.0_linux_x86_64_MPI/bin/raxml-ng-mpi -all -msa yellow_filtered_coleopt_hymenopt_chely_bact_Drosophila_renamed_alignment.phy --prefix yellow_all --threads 16 -model LG+G4
+raxml-ng-mpi -all -msa yellow_filtered_coleopt_hymenopt_chely_bact_Drosophila_renamed_alignment.phy --prefix yellow_all --threads 16 -model LG+G4
 
 
