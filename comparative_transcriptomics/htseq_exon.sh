e8d03f36df09d88f0d48f7c7ecbb43748b81023d
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:25:32 +0100

    Update htseq_exon.sh

--- comparative_transcriptomics/htseq_exon.sh
+++ comparative_transcriptomics/htseq_exon.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N htseq_lifecyle
+#$ -N htseq_all
 #$ -cwd
 
 source ~/.bashrc
 
+gff=Chelymorpha_alternans.gff3
 
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Adult_FG_1_sorted.bam Chelymorpha_alternans.gff3 > count_table_Adult_FG_1_exon_Parent_new_ann.txt
+for bam in ../*_sorted.bam; do
+    sample=$(basename "$bam" _sorted.bam)
+    out="count_table_${sample}_exon_Parent_new_ann.txt"
 
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Adult_FG_2_sorted.bam Chelymorpha_alternans.gff3 > count_table_Adult_FG_2_exon_Parent_new_ann.txt
+    echo "Processing $sample"
 
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Adult_FG_3_sorted.bam Chelymorpha_alternans.gff3 > count_table_Adult_FG_3_exon_Parent_new_ann.txt
-
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Adult_OV_1_sorted.bam Chelymorpha_alternans.gff3 > count_table_Adult_OV_1_exon_Parent_new_ann.txt
-
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Adult_OV_2_sorted.bam Chelymorpha_alternans.gff3 > count_table_Adult_OV_2_exon_Parent_new_ann.txt
-
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Adult_OV_3_sorted.bam Chelymorpha_alternans.gff3 > count_table_Adult_OV_3_exon_Parent_new_ann.txt
-
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Larvae_1_sorted.bam Chelymorpha_alternans.gff3 > count_table_Larvae_1_exon_Parent_new_ann.txt
-
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Larvae_2_sorted.bam Chelymorpha_alternans.gff3 > count_table_Larvae_2_exon_Parent_new_ann.txt
-
-htseq-count -f bam -t exon --stranded=reverse -r pos -i Parent ../Larvae_3_sorted.bam Chelymorpha_alternans.gff3 > count_table_Larvae_3_exon_Parent_new_ann.txt
-
+    htseq-count \
+      -f bam \
+      -t exon \
+      --stranded=reverse \
+      -r pos \
+      -i Parent \
+      "$bam" \
+      "$gff" \
+      > "$out"
+done
