73bac2dc9241b06d2965689c71ad4feb42e2ba2e
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:27:55 +0100

    Update partitionfinder.sh

--- phylogenies/Stammera_phylogeny/partitionfinder.sh
+++ phylogenies/Stammera_phylogeny/partitionfinder.sh
@@ -9,4 +9,4 @@ source ~/.bashrc
 conda activate py27
 set NUMEXPR_MAX_THREADS=128
 
-python /ebio/ag-salem/projects/Metagenomic_assemblies/code/partitionfinder-2.1.1/PartitionFinderProtein.py --raxml partitionfinder -p 128 --rcluster-max 100
+python PartitionFinderProtein.py --raxml partitionfinder -p 128 --rcluster-max 100
