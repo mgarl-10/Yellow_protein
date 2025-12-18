b0693a559aa4f587e7686c82374a56d364bf95a2
mgarl <88340540+mgarl-10@users.noreply.github.com>
2025-12-18 11:28:10 +0100

    Update raxml.sh

--- phylogenies/Stammera_phylogeny/raxml.sh
+++ phylogenies/Stammera_phylogeny/raxml.sh
@@ -3,7 +3,6 @@
 #$ -N raxml_HPC
 #$ -cwd
 
-
-/ebio/ag-salem/projects/Metagenomic_assemblies/code/raxml-ng_v1.2.0_linux_x86_64_MPI/bin/raxml-ng-mpi -all -msa concatenated_aln_yellow_species.phy --prefix Stammera_yellow_species_protein --threads 16 -model partitions.txt --data-type AA --tree pars{25},rand{25} 
+raxml-ng-mpi -all -msa concatenated_aln_yellow_species.phy --prefix Stammera_yellow_species_protein --threads 16 -model partitions.txt --data-type AA --tree pars{25},rand{25} 
 
 
