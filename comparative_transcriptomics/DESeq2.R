f83c168eee409fa844926630795ccbbf3caea2a2
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-17 11:54:56 +0100

    Update DESeq2.R

--- comparative_transcriptomics/DESeq2.R
+++ comparative_transcriptomics/DESeq2.R
@@ -35,28 +35,22 @@ Adult_OV_Adult_FG<-results(dds, contrast=c("Treatment","Adult_OV","Adult_FG"))
 
 #########green#############
 # Load required libraries
-library(DESeq2)         # Ensure DESeq2 is installed
-library(EnhancedVolcano) # Ensure EnhancedVolcano is installed
+library(DESeq2)        
+library(EnhancedVolcano)
 
-# Generate the results (Make sure 'dds' is already defined)
 Adult_OV_Adult_FG <- results(dds, contrast = c("Treatment", "Adult_OV", "Adult_FG"))
-
-# Check if the results contain NA values and handle them if necessary
 Adult_OV_Adult_FG <- na.omit(Adult_OV_Adult_FG)
 
-# Define the colors using rgb()
 green_color <- rgb(5, 143, 77, maxColorValue = 255)
 yellow_color <- rgb(247, 182, 39, maxColorValue = 255)
 
-# Create a color vector based on whether the gene is the one to highlight
 color_vector <- ifelse(rownames(Adult_OV_Adult_FG) == 'FUN_023368-T1', yellow_color, green_color)
 
-# Create the volcano plot
 EnhancedVolcano(Adult_OV_Adult_FG,
    lab = rownames(Adult_OV_Adult_FG),
    x = 'log2FoldChange',
    y = 'padj',
-   selectLab = c('FUN_023368-T1'),  # Highlight the specific gene
+   selectLab = c('FUN_023368-T1'), 
    pCutoff = 5e-2,
    FCcutoff = 2.0,
    pointSize = 4,
@@ -65,15 +59,15 @@ EnhancedVolcano(Adult_OV_Adult_FG,
    labFace = 'bold',
    boxedLabels = TRUE,
    colAlpha = 4/5,
-   legendPosition = 'none',  # Remove the legend
+   legendPosition = 'none',  
    drawConnectors = TRUE,
    widthConnectors = 1.0,
    colConnectors = 'black',
-   gridlines.major = FALSE,  # Remove major gridlines
-   gridlines.minor = FALSE,  # Remove minor gridlines
-   col = color_vector,        # Set the colors based on the condition
-   xlab = expression(Log[2]~ 'fold change'),  # Keep x-axis label
-   ylab = expression(-Log[10]~ 'p-value'),      # Keep y-axis label
-   title = NULL               # Remove the title
+   gridlines.major = FALSE, 
+   gridlines.minor = FALSE,  
+   col = color_vector,       
+   xlab = expression(Log[2]~ 'fold change'), 
+   ylab = expression(-Log[10]~ 'p-value'),  
+   title = NULL            
 )
 
