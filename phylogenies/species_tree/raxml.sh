1cb2c91b83ba65554174dd028967360c89f7da83
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-17 11:35:57 +0100

    Delete phylogenies/species_tree directory

--- phylogenies/species_tree/raxml.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -S /bin/bash
-#$ -N raxml_species
-#$ -cwd
-
-
-raxml-ng-mpi -all -msa concatenated_alignment_7species.phy --prefix host_7species_unrooted --threads 16 -model partitions.txt --tree pars{25},rand{25} 
-
-
