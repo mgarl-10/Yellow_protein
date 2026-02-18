02b2781a682bd369caa98d8258ebe9fe37b6a58c
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 15:54:38 +0100

    Update blast.sh

--- genome_assembly/blast.sh
+++ genome_assembly/blast.sh
@@ -4,4 +4,4 @@
 #$ -cwd
 
 
-blastn -query Chelymorpha_alternans_hifiasm_assembly.fasta -db nt -outfmt "6 qseqid staxids bitscore std" -max_target_seqs 1 -max_hsps 1 -num_threads 64 -evalue 1e-25 -out C_alternans_assembly_contaminants.txt
+blastn -query Chelymorpha_alternans_hifiasm_redun_assembly.fasta -db nt -outfmt "6 qseqid staxids bitscore std" -max_target_seqs 1 -max_hsps 1 -num_threads 64 -evalue 1e-25 -out C_alternans_assembly_contaminants.txt
