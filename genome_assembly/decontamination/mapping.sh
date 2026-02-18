8f23960a87f16e99ef7bcee0d0c5bf11099db94c
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 16:13:33 +0100

    Update mapping.sh

--- genome_assembly/decontamination/mapping.sh
+++ genome_assembly/decontamination/mapping.sh
@@ -4,6 +4,7 @@
 #$ -cwd
 
 bwa index Chelymorpha_alternans_hifiasm_redun_assembly.fasta
+
 bwa mem -t 64 Chelymorpha_alternans_hifiasm_redun_assembly.fasta Chelymorpha_alternans_ccs.fasta > mapping_Chelymorpha_alternans.sam
 
 samtools sort --threads 64 mapping_Chelymorpha_alternans.sam > mapping_Chelymorpha_alternans.bam 
