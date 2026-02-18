bf9cb7016c265e04b1e72cfdb6850a5f4af14d6d
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 15:53:54 +0100

    Update mapping.sh

--- genome_assembly/mapping.sh
+++ genome_assembly/mapping.sh
@@ -3,8 +3,8 @@
 #$ -N bwa
 #$ -cwd
 
-bwa index Chelymorpha_alternans_hifiasm_assembly.fasta
-bwa mem -t 64 Chelymorpha_alternans_hifiasm_assembly.fasta Chelymorpha_alternans_ccs.fasta > mapping_Chelymorpha_alternans.sam
+bwa index Chelymorpha_alternans_hifiasm_redun_assembly.fasta
+bwa mem -t 64 Chelymorpha_alternans_hifiasm_redun_assembly.fasta Chelymorpha_alternans_ccs.fasta > mapping_Chelymorpha_alternans.sam
 
 samtools sort --threads 64 mapping_Chelymorpha_alternans.sam > mapping_Chelymorpha_alternans.bam 
 
