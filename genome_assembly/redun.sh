12ca1df9ef4db8884e1b62e0831c3c261e4447ca
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 15:54:22 +0100

    Update redun.sh

--- genome_assembly/redun.sh
+++ genome_assembly/redun.sh
@@ -7,4 +7,4 @@
 source ~/.bashrc
 conda activate py27
 
-python /code/redundans/redundans.py -i RNAseq_reads_1.fastq RNAseq_reads_2.fastq -l Chelymorpha_alternans_ccs.fasta -f Chelymorpha_alternans_hifiasm_assembly.fasta -o Chelymorpha_alternans_hifiasm_redundans -t 128 --log LOG
+python /code/redundans/redundans.py -i RNAseq_reads_1.fastq RNAseq_reads_2.fastq -l Chelymorpha_alternans_ccs.fasta -f Chelymorpha_alternans_hifiasm_assembly.fasta -o Chelymorpha_alternans_hifiasm_redun -t 128 --log LOG
