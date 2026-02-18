8d422fb7149d6b568d810d90d3a4939746612daa
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 16:15:40 +0100

    Update repeatmasker.sh

--- genome_assembly/annotation/repeatmasker.sh
+++ genome_assembly/annotation/repeatmasker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N rptm_Crubi
+#$ -N rptm_Calt
 #$ -cwd
 
 
 
-#/ebio/ag-salem/projects/BeetleGenomes/code/RepeatModeler-2.0.3/BuildDatabase -name C_rubiginosa -engine ncbi C_rubiginosa_genome_cleaned_sorted.fa > database_build_C_rubiginosa_run.out 
+/code/RepeatModeler-2.0.3/BuildDatabase -name C_alternans -engine ncbi C_alternans_genome_cleaned_sorted.fa > database_build_C_alternans_run.out 
 
-#/ebio/ag-salem/projects/BeetleGenomes/code/RepeatModeler-2.0.3/RepeatModeler -database C_rubiginosa -engine ncbi -pa 32 -LTRStruct > C_rubiginosa_run_repeatmodeler.out 
+/code/RepeatModeler-2.0.3/RepeatModeler -database C_alternans -engine ncbi -pa 32 -LTRStruct > C_alternans_run_repeatmodeler.out 
 
-/ebio/ag-salem/projects/BeetleGenomes/code/maker/exe/RepeatMasker/RepeatMasker -pa 64 -gff -lib ./RM_106778.ThuFeb151425062024/consensi.fa.classified C_rubiginosa_genome_cleaned_sorted.fa > repeatmasker.out
+/code/RepeatMasker/RepeatMasker -pa 64 -gff -lib ./RM/consensi.fa.classified C_alternans_genome_cleaned_sorted.fa > repeatmasker.out
