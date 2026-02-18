3a0079b1a74a78e65f193872207fb6b9d9ec2b93
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 16:11:35 +0100

    Update busco_C_alt.sh

--- genome_assembly/busco_C_alt.sh
+++ genome_assembly/busco_C_alt.sh
@@ -4,8 +4,8 @@
 #$ -cwd
 
 
-source /ebio/ag-salem/projects/CassidinaeGenomics/code/minconda3/etc/profile.d/conda.sh
+source ~/.bashrc
 conda activate busco
 
-/ebio/ag-salem/projects/CassidinaeGenomics/code/minconda3/envs/busco/bin/busco -m genome -i contigs.reduced.fa -o busco_redundans_hifiasm -l /ebio/ag-salem/projects/CassidinaeGenomics/code/minconda3/envs/busco/busco_downloads/lineages/endopterygota_odb10 -f --offline --download_path /ebio/ag-salem/projects/CassidinaeGenomics/code/minconda3/envs/busco/busco_downloads/ -c 64
+busco -m genome -i Chelymorpha_alternans_hifiasm_redun.fasta -o busco_redundans_hifiasm -l endopterygota_odb10 -f --offline --download_path /busco/busco_downloads/ -c 64
 
