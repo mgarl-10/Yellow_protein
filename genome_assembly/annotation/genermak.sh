058047f2c1e643949cb25c6184e6b6d2323e30b7
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 16:25:31 +0100

    Update genermak.sh

--- genome_assembly/annotation/genermak.sh
+++ genome_assembly/annotation/genermak.sh
@@ -7,5 +7,5 @@
 source ~/.bashrc
 
 
-/ebio/ag-salem/projects/BeetleGenomes_Annotation/code/maker/exe/gmes_linux_64/gmes_petap.pl --ES --max_intron 460000 --soft_mask 2000 --cores 64 --sequence ../C_rubiginosa_assembly.fa
+/code/gmes_linux_64/gmes_petap.pl --ES --max_intron 460000 --soft_mask 2000 --cores 64 --sequence C_alternans_assembly.fa
 
