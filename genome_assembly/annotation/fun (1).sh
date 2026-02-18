205a9b8860ac779719c61eac695da6ca3106f273
Marleny García-Lozano <88340540+mgarl-10@users.noreply.github.com>
2026-02-18 16:24:16 +0100

    Update fun (1).sh

--- genome_assembly/annotation/fun (1).sh
+++ genome_assembly/annotation/fun (1).sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -N fun_Crubi
+#$ -N fun_Calt
 #$ -cwd
 
 
@@ -9,16 +9,16 @@ source ~/.bashrc
 conda activate funannotate
 
 
-#funannotate mask -i C_rubiginosa_genome_cleaned_sorted.fa -m repeatmasker -l ./RM_95950.WedFeb211359442024/consensi.fa.classified --cpus 64 -o C_rubiginosa_assembly.fa
-
-#funannotate train -i C_rubiginosa_assembly.fa -o fun --left /ebio/ag-salem/projects/CassidinaeGenomics/Marleny_Crubiginosa/RNA_S15195Nr9.1.fastq.gz --right /ebio/ag-salem/projects/CassidinaeGenomics/Marleny_Crubiginosa/RNA_S15195Nr9.2.fastq.gz --stranded RF --species "Cassida rubiginosa" --cpus 32 --max_intronlen 460000 --memory 100G
+funannotate train -i C_alternans_assembly.fa -o fun --left RNAseq_reads.1.fastq.gz --right RNAseq_reads.1.fastq.gz --stranded RF --species "Chelymorpha alternans" --cpus 32 --max_intronlen 460000 --memory 100G
 
 
-#funannotate predict -i C_rubiginosa_assembly.fa -o fun -s "Cassida rubiginosa" --max_intronlen 460000 --organism other --repeats2evm --busco_db endopterygota --weights glimmerhmm:0 --genemark_gtf /ebio/ag-salem/projects/CassidinaeGenomics/data/C_rubiginosa_Annotation/funannotate/genemark/genemark.gtf
+funannotate predict -i C_alternans_assembly.fa -o fun -s "Chelymorpha_alternans" --max_intronlen 460000 --organism other --repeats2evm --busco_db endopterygota --weights glimmerhmm:0 --genemark_gtf genemark.gtf
 
 
-#funannotate update -i fun  --cpus 64
+funannotate update -i fun  --cpus 64
 
-#funannotate species -s cassida_rubiginosa -a fun/predict_results/cassida_rubiginosa.parameters.json
-#funannotate iprscan -i fun -m local --iprscan_path /ebio/ag-salem/projects/BeetleGenomes_Annotation/code/my_interproscan/interproscan-5.55-88.0/interproscan.sh -c 64
-funannotate annotate -i fun --species "Cassida rubiginosa" --busco_db endopterygota --cpus 64
+funannotate species -s chelymorpha_alternans -a fun/predict_results/cassida_rubiginosa.parameters.json
+
+funannotate iprscan -i fun -m local --iprscan_path /code/interproscan/interproscan.sh -c 64
+
+funannotate annotate -i fun --species "Chelymorpha alternans" --busco_db endopterygota --cpus 64
